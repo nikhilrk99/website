52c695f0438f4c6fd6057ab59fc183c6610fa62a
Nikhil <nikhilrk99@gmail.com>
2026-08-06 15:52:17 -0500

    feat: added resume and calendar links

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -1,6 +1,6 @@
 // TODO: replace with real values.
 export const siteConfig = {
   name: "Nikhil Koundinya",
-  calendarUrl: "#", // TODO: add Google Calendar scheduling link
-  resumeUrl: "#", // TODO: add resume PDF path (e.g. /resume.pdf)
+  calendarUrl: "https://calendar.app.google/ikmka28QcaGtDM6GA", // TODO: add Google Calendar scheduling link
+  resumeUrl: "https://nikhilkoundinya.com/assets/files/resume.pdf", // TODO: add resume PDF path (e.g. /resume.pdf)
 };
