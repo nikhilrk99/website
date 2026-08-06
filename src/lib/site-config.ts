3b244d276e4072bff920858464ff8ca37dc6191b
Nikhil <nikhilrk99@gmail.com>
2026-08-06 15:52:56 -0500

    chore: remove comments in site config

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -1,6 +1,6 @@
-// TODO: replace with real values.
+
 export const siteConfig = {
   name: "Nikhil Koundinya",
-  calendarUrl: "https://calendar.app.google/ikmka28QcaGtDM6GA", // TODO: add Google Calendar scheduling link
-  resumeUrl: "https://nikhilkoundinya.com/assets/files/resume.pdf", // TODO: add resume PDF path (e.g. /resume.pdf)
+  calendarUrl: "https://calendar.app.google/ikmka28QcaGtDM6GA", =
+  resumeUrl: "https://nikhilkoundinya.com/assets/files/resume.pdf", 
 };
